b8edb8d240859f5f3611408146267140af1583e5
AshwinJacobs <ashwin.dean21@gmail.com>
2022-08-11 09:55:36 +0200

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,19 +1,1 @@
-<template>
-  <div class="video-container">
-    <video
-      src="https://traversymedia.com/downloads/video.mov"
-      autoplay
-      muted
-      loop
-    ></video>
-  </div>
-  <div class="content">
-    <h1>Shoot For The Stars</h1>
-    <h3>THE PRODUCT STORE</h3>
-    <!-- <a href="./index.html" class="btn">TABLES</a> -->
 
-    <a href="./login_form.html" class="btn">LOGIN</a>
-  </div>
-</template>
-
-<style scoped></style>
